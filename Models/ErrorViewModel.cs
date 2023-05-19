9aa2462870c72ae58bae01d7ea0f2bc70b5702f8
Lakshmikasireddy <133949582+Lakshmikasireddy@users.noreply.github.com>
2023-05-19 17:01:19 +0530

    test

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace azuretraintest.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
